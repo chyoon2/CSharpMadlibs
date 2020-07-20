3c5c4873011224b1593bf1f6c4f6114c28c57f87
Taylor Somers <tsomers@yandex.com>
2020-07-20 14:23:54 -0400

    Add Controllers, MadlibsWords, and additional homepage functionality
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- Madlibs/Controllers/HomeControllers.cs
+++ Madlibs/Controllers/HomeControllers.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Madlibs.Models;
 
-namespace FriendLetter.Controllers
+namespace Madlibs.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
+    
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    public ActionResult Madpage() { 
+      MadlibsWords myWord = new MadlibsWords();
+      myWord.Noun = "Garage Door";
+      return View(myWord);
+    }
   }
 }
