141f4995e1b1f40cd9d8696b93be3beb0d468cd6
Taylor Somers <tsomers@yandex.com>
2020-07-20 16:41:34 -0400

    Add remaining necessary code to fill in madlib form completely with user inputs
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- Madlibs/Controllers/HomeControllers.cs
+++ Madlibs/Controllers/HomeControllers.cs
@@ -15,11 +15,17 @@ namespace Madlibs.Controllers
       return View(myWord);
     }
     [HttpPost("/madform")]
-    public ActionResult Madform(string noun)
+    public ActionResult Madform(string noun, string noun2, string noun3, string verb, string adverb, string adjective, string adjective2)
     {
       MadlibsWords myWord = new MadlibsWords();
       myWord.Noun = noun;
-      return View("Madpage", myWord);
+      myWord.Noun2 = noun2;
+      myWord.Noun3 = noun3;
+      myWord.Verb = verb;
+      myWord.Adverb = adverb;
+      myWord.Adjective = adjective;
+      myWord.Adjective2 = adjective2;
+      return View(myWord);
     }
 
   }
