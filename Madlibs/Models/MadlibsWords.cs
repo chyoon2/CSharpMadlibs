96ca452572c8ba6bc697add28c2a64f0d044e115
Taylor Somers <tsomers@yandex.com>
2020-07-20 18:06:14 -0400

    Finalize functionality for additional madlibs form and output display
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- Madlibs/Models/MadlibsWords.cs
+++ Madlibs/Models/MadlibsWords.cs
@@ -3,11 +3,11 @@ namespace Madlibs.Models
   public class MadlibsWords
   {
     public string Noun { get; set; }
-
+    public string Noun2 { get; set; }
+    public string Noun3 { get; set; }
     public string Verb { get; set; }
-
     public string Adjective { get; set; }
-
+    public string Adjective2 { get; set; }
     public string Adverb { get; set; }
   }
 }
