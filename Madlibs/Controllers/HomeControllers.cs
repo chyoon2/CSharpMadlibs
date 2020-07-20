87bf17ea9264618074d315971f8465b4bd0944dd
Taylor Somers <tsomers@yandex.com>
2020-07-20 17:24:04 -0400

    Add additional forms and UI output pages for Pizza madlib
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- Madlibs/Controllers/HomeControllers.cs
+++ Madlibs/Controllers/HomeControllers.cs
@@ -5,15 +5,30 @@ namespace Madlibs.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/zooform")]
-    public ActionResult ZooForm() { return View(); }
-
     [HttpGet("/")]
     public ActionResult Madpage() { 
       MadlibsWords myWord = new MadlibsWords();
-      // myWord.Noun = "Garage Door";
       return View(myWord);
     }
+
+    [HttpGet("/pizzaform")]
+    public ActionResult PizzaForm() { return View(); }
+
+    [HttpPost("/madpizza")]
+    public ActionResult MadPizza(string noun, string noun2, string noun3, string adjective, string adjective2)
+    {
+      MadlibsWords myWord = new MadlibsWords();
+      myWord.Noun = noun;
+      myWord.Noun2 = noun2;
+      myWord.Noun3 = noun3;
+      myWord.Adjective = adjective;
+      myWord.Adjective2 = adjective2;
+      return View(myWord);
+    }
+
+    [HttpGet("/zooform")]
+    public ActionResult ZooForm() { return View(); }
+
     [HttpPost("/madzoo")]
     public ActionResult MadZoo(string noun, string noun2, string noun3, string verb, string adverb, string adjective, string adjective2)
     {
